c23e8641db00c48cba8f483f14889d7bc1d2d4aa
brendanrmills <bmills@lco.global>
2022-06-29 15:13:46 -0700

    added the targetClasification model

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tom_classifications
+
+Classifications!
+
+This is an app designed to support different ways a broker can classify a target using the TOM
+
+The documentation and a more detailed readme will come later.
---
+++ classifications/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from tom_targets.models import Target
+
+class TargetClassification(models.Model):
+    '''
+    Class representing a broker's classification of a target.
+
+    :param target: The Target that this classification is assciated with.
+    :type target: Target
+
+    '''
+    target = models.ForeignKey(Target, on_delete=models.CASCADE)
+    source = models.TextField(blank=True, default='')
+    level = models.TextField(blank=True, default='')
+    classification = models.TextField(blank=True, default='')
+    probability = models.FloatField(null=True, blank=True)
+    mjd = models.FloatField(null=True, blank=True)
+
+    def as_dict(self):
+        return {
+            'broker': self.source,
+            'level': self.level,
+            'type': self.classification,
+            'prob': self.probability,
+            'mjd': self.mjd
+        }
