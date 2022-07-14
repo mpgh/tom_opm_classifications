a6ce26f0e8cf2125e85ef1c2932a6970ee9fb55d
brendanrmills <bmills@lco.global>
2022-07-14 14:24:18 -0700

    added transient

--- tom_classifications/apps.py
+++ tom_classifications/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ClassificationsConfig(AppConfig):
+class Tom_classificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'classifications'
+    name = 'tom_classifications'
